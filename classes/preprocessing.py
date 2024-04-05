fc0df5668930144116b456f5a4f1ac328b435404
Jack Pay <jack.pay@student.manchester.ac.uk>
2024-04-05 16:51:06 +0100

    Refactor load csv data & apply black

--- classes/preprocessing.py
+++ classes/preprocessing.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 import pandas as pd
 import os
 
+
 @dataclass(frozen=True)
 class DatasetKeys:
     """
@@ -23,13 +24,22 @@ class PathKeys:
     TRIAL_FILEPATH: str = "data/trial_data/trial_data"
     TRIAL_DATASET: str = f"{TRIAL_FILEPATH}/NLI_trial.csv"
 
-def load_data(data_dir: str = "./") -> typing.Tuple[typing.Tuple[typing.List[str], typing.List[str], typing.List[int]], typing.Tuple[typing.List[str], typing.List[str], typing.List[int]]]:
+
+def load_data(
+    data_dir: str = "./",
+) -> typing.Tuple[
+    typing.Tuple[typing.List[str], typing.List[str], typing.List[int]],
+    typing.Tuple[typing.List[str], typing.List[str], typing.List[int]],
+]:
     """
     Will load in both the training & trial data based on the data directory
-    
+
     data_dir:      String directory to the location of the data
     """
-    return (load_data_csv(filepath=os.path.join(data_dir, PathKeys.TRAIN_DATASET)), load_data_csv(filepath=os.path.join(data_dir, PathKeys.DEV_DATASET)))
+    return (
+        load_data_csv(filepath=os.path.join(data_dir, PathKeys.TRAIN_DATASET)),
+        load_data_csv(filepath=os.path.join(data_dir, PathKeys.DEV_DATASET)),
+    )
 
 
 def load_data_csv(
@@ -38,8 +48,8 @@ def load_data_csv(
     """
     Will load in data from the csv filepath specified. Expects the string filepath to a csv file. Returns tuple of the premises, hypotheses and labels
     """
-    dataset = pd.read_csv(filepath).to_dict()
-    premises = list(map(str, dataset[DatasetKeys.PREMISE_KEY].values()))
-    hypotheses = list(map(str, dataset[DatasetKeys.HYPOTHESIS_KEY].values()))
-    labels = list(map(int, dataset[DatasetKeys.LABEL_KEY].values()))
+    dataset = pd.read_csv(filepath)
+    premises = dataset[DatasetKeys.PREMISE_KEY].astype(str).tolist()
+    hypotheses = dataset[DatasetKeys.HYPOTHESIS_KEY].astype(str).tolist()
+    labels = dataset[DatasetKeys.LABEL_KEY].astype(str).tolist()
     return (premises, hypotheses, labels)
