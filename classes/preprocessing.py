a0fafff794f44413e15c111fb7be2adf18b601cf
Jack Pay <jack.pay@student.manchester.ac.uk>
2024-04-05 14:29:15 +0100

    Add new method to load all data in one call, #3

--- classes/preprocessing.py
+++ classes/preprocessing.py
@@ -1,7 +1,7 @@
 import typing
 from dataclasses import dataclass
 import pandas as pd
-
+import os
 
 @dataclass(frozen=True)
 class DatasetKeys:
@@ -12,6 +12,24 @@ class DatasetKeys:
     PREMISE_KEY: str = "premise"
     HYPOTHESIS_KEY: str = "hypothesis"
     LABEL_KEY: str = "label"
+
+
+@dataclass(frozen=True)
+class PathKeys:
+    TRAIN_FILEPATH: str = "data/training_data/training_data/NLI"
+    TRAIN_DATASET: str = f"{TRAIN_FILEPATH}/train.csv"
+    DEV_DATASET: str = f"{TRAIN_FILEPATH}/dev.csv"
+
+    TRIAL_FILEPATH: str = "data/trial_data/trial_data"
+    TRIAL_DATASET: str = f"{TRIAL_FILEPATH}/NLI_trial.csv"
+
+def load_data(data_dir: str = "./") -> typing.Tuple[typing.Tuple[typing.List[str], typing.List[str], typing.List[int]], typing.Tuple[typing.List[str], typing.List[str], typing.List[int]]]:
+    """
+    Will load in both the training & trial data based on the data directory
+    
+    data_dir:      String directory to the location of the data
+    """
+    return (load_data_csv(filepath=os.path.join(data_dir, PathKeys.TRAIN_DATASET)), load_data_csv(filepath=os.path.join(data_dir, PathKeys.DEV_DATASET)))
 
 
 def load_data_csv(
